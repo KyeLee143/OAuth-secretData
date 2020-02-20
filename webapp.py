17d002fd66a6a95b351b4945cbf7061e3269ff99
KyeLee143 <54326810+KyeLee143@users.noreply.github.com>
2020-02-20 09:25:25 -0800

    Update webapp.py
    Got rid of page2.html

--- webapp.py
+++ webapp.py
@@ -76,13 +76,6 @@ def renderPage1():
         user_data_pprint = '';
     return render_template('page1.html',dump_user_data=user_data_pprint)
 
-@app.route('/page2')
-def renderPage2():
-    if 'user_data' in session:
-        user_data_pprint = pprint.pformat(session['user_data']['public_repos'])#format the user data nicely
-    else:
-        user_data_pprint = '';
-    return render_template('page2.html',dump_user_data=user_data_pprint)
 
 @github.tokengetter
 def get_github_oauth_token():
